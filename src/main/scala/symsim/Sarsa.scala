b142f9042ca3c86f76d00302d6172ce23c2a471b
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-04-23 20:53:35 +0200

    Make scala3 accept our code without the migration mode

--- src/main/scala/symsim/Sarsa.scala
+++ src/main/scala/symsim/Sarsa.scala
@@ -22,140 +22,140 @@ import org.typelevel.paiges.Doc
 trait Sarsa[State, FiniteState, Action, Reward, Scheduler[_]]
   extends RL[FiniteState, Action] {
 
-    type A = Agent[State, FiniteState, Action, Reward, Scheduler]
+  type A = Agent[State, FiniteState, Action, Reward, Scheduler]
 
-    val agent: A
+  val agent: A
 
-    import agent.instances._
+  import agent.instances._
 
-    type Q = Map[FiniteState, Map[Action, Reward]]
+  type Q = Map[FiniteState, Map[Action, Reward]]
 
-    def alpha: Double
-    def gamma: Double
+  def alpha: Double
+  def gamma: Double
 
-    def bestAction (q: Q) (s: State): Action
+  def bestAction (q: Q) (s: State): Action
 
-    def chooseAction (q: Q) (s: State): Scheduler[Action]
-
-
-    /** A single step of the learning algorithm
-      *
-      * @param q the last Q-matrix
-      * @param s_t current state
-      * @return the updated matrix Q, the successor state, and a
-      * reward difference (the size of the update performed)
-      */
-    def learn1 (q: Q, s_t: State): Scheduler[(Q,State)] = for {
-
-      a_t <- chooseAction (q) (s_t)
-
-      (s_tt, r_tt) = agent.step (s_t) (a_t)
-
-      a_tt = bestAction (q) (s_tt)
-
-      // concerned that this is Q-learning not SARSA (p.844 in Russel & Norvig)
-      ds_t = agent.discretize (s_t)
-      ds_tt = agent.discretize (s_tt)
-      old_entry = q (ds_t) (a_t)
-      correction = r_tt + gamma * q (ds_tt) (a_tt) - old_entry
-      qval = old_entry + alpha * correction
-
-      q1 = q + (ds_t -> (q (ds_t) + (a_t -> qval)))
-
-    } yield (q1, s_tt)
+  def chooseAction (q: Q) (s: State): Scheduler[Action]
 
 
+  /** A single step of the learning algorithm
+    *
+    * @param q the last Q-matrix
+    * @param s_t current state
+    * @return the updated matrix Q, the successor state, and a
+    * reward difference (the size of the update performed)
+    */
+  def learn1 (q: Q, s_t: State): Scheduler[(Q,State)] = for {
 
-    /** Execute a full learning episode (until the final state of agent is
-      * reached).
-      */
-    def learn (q: Q, s_t: State): Scheduler[Q] = {
-      val initial = q -> s_t
-      val f = (learn1 _).tupled
-      val p = { (qs: (Q,State)) => agent.isFinal (qs._2) }
-      Monad[Scheduler]
-        .iterateUntilM[(Q,State)] (initial) (f) (p)
-        .map { _._1 }
-    }
+    a_t <- chooseAction (q) (s_t)
+
+    (s_tt, r_tt) = agent.step (s_t) (a_t)
+
+    a_tt = bestAction (q) (s_tt)
+
+    // concerned that this is Q-learning not SARSA (p.844 in Russel & Norvig)
+    ds_t = agent.discretize (s_t)
+    ds_tt = agent.discretize (s_tt)
+    old_entry = q (ds_t) (a_t)
+    correction = r_tt + gamma * q (ds_tt) (a_tt) - old_entry
+    qval = old_entry + alpha * correction
+
+    q1 = q + (ds_t -> (q (ds_t) + (a_t -> qval)))
+
+  } yield (q1, s_tt)
 
 
 
-    /** Execute a full  learning episode from initial  state (until the
-      * final state of agent is reached).
-      */
-    def learn (q: Q): Scheduler[Q] =
+  /** Execute a full learning episode (until the final state of agent is
+    * reached).
+    */
+  def learn (q: Q, s_t: State): Scheduler[Q] = {
+    val initial = q -> s_t
+    val f = (learn1 _).tupled
+    val p = { (qs: (Q,State)) => agent.isFinal (qs._2) }
+    Monad[Scheduler]
+      .iterateUntilM[(Q,State)] (initial) (f) (p)
+      .map { _._1 }
+  }
 
+
+
+  /** Execute a full  learning episode from initial  state (until the
+    * final state of agent is reached).
+    */
+  def learn (q: Q): Scheduler[Q] =
+
+    for {
+      s0 <- agent.initialize
+      result <- learn (q, s0)
+    } yield  result
+
+
+  /** Construct a zero initialized Q matrix */
+  def initQ: Q
+
+
+  /**
+   * Execute 'n' full learning episodes (until the final state of agent is
+   * reached), starting with the matrix q
+   */
+  def learnN (n: Int, q: Q) (implicit ar: Arith[Reward]): Scheduler[Q] = {
+
+    // The endomonoid for Kleisli[Scheduler,QS,QS], apparently not automatic
+    type EndoKleisli[A] = Kleisli[Scheduler,A,A]
+    def endoKleisli[A] (f: A => Scheduler[A]) = Kleisli[Scheduler,A,A] (f)
+    implicit val monoid: MonoidK[EndoKleisli] = Kleisli.endoMonoidK[Scheduler]
+
+    val l: Q => Scheduler[Q] = learn _
+
+    val q1 = LazyList
+      // Prepare 'epoch'-many learning steps
+      .fill (n) (l)
+      // Wrap each in Kleisli[Q => Randomized[Q]} and compose them in the monoid
+      .foldMapK[EndoKleisli, Q] (endoKleisli[Q])
+      // Got a single Kleisli[Q => Randomized[Q]], run it from initial conditions
+      .run (q)
+
+    q1
+
+  }
+
+
+
+  /** Convert the matrix Q after training into a Policy map */
+  def qToPolicy (q: Q) (implicit order: Ordering[Reward]): Policy = {
+
+    def best (m: Map[Action,Reward]): Action =
+      m.map { _.swap } (m.values.max)
+
+    q.view.mapValues (best).to (Map)
+  }
+
+  /** Generate total Q matrices for testing. */
+  val genQ: Gen[Q] = {
+
+    val as = agent.instances.allActions
+    val genReward = agent.instances.arbitraryReward.arbitrary
+
+    val genActionReward: Gen[Map[Action,Reward]] =
       for {
-        s0 <- agent.initialize
-        result <- learn (q, s0)
-      } yield  result
+        // TODO refactor, seek what is available for maps
+        rewards <- Gen.sequence[List[Reward],Reward]
+          { List.fill (as.size) (genReward) }
+        ars = as zip rewards
+      } yield Map (ars: _*)
 
+    val fs = agent.instances.allFiniteStates
+    val genStateActionRewards: Gen[Q] =
+      for  {
+        // TODO refactor, seek what is available for maps
+        mars <- Gen.sequence[List[Map[Action,Reward]], Map[Action,Reward]]
+          { List.fill (fs.size) (genActionReward) }
+        smars = fs zip mars
+      } yield Map (smars: _*)
 
-    /** Construct a zero initialized Q matrix */
-    def initQ: Q
-
-
-    /**
-     * Execute 'n' full learning episodes (until the final state of agent is
-     * reached), starting with the matrix q
-     */
-    def learnN (n: Int, q: Q) (implicit ar: Arith[Reward]): Scheduler[Q] = {
-
-      // The endomonoid for Kleisli[Scheduler,QS,QS], apparently not automatic
-      type EndoKleisli[A] = Kleisli[Scheduler,A,A]
-      def endoKleisli[A] (f: A => Scheduler[A]) = Kleisli[Scheduler,A,A] (f)
-      implicit val monoid: MonoidK[EndoKleisli] = Kleisli.endoMonoidK[Scheduler]
-
-      val l: Q => Scheduler[Q] = learn _
-
-      val q1 = LazyList
-        // Prepare 'epoch'-many learning steps
-        .fill (n) (l)
-        // Wrap each in Kleisli[Q => Randomized[Q]} and compose them in the monoid
-        .foldMapK[EndoKleisli, Q] (endoKleisli[Q])
-        // Got a single Kleisli[Q => Randomized[Q]], run it from initial conditions
-        .run (q)
-
-      q1
-
-    }
-
-
-
-    /** Convert the matrix Q after training into a Policy map */
-    def qToPolicy (q: Q) (implicit order: Ordering[Reward]): Policy = {
-
-      def best (m: Map[Action,Reward]): Action =
-        m.map { _.swap } (m.values.max)
-
-      q.view.mapValues (best).to (Map)
-    }
-
-    /** Generate total Q matrices for testing. */
-    val genQ: Gen[Q] = {
-
-      val as = agent.instances.allActions
-      val genReward = agent.instances.arbitraryReward.arbitrary
-
-      val genActionReward: Gen[Map[Action,Reward]] =
-        for {
-          // TODO refactor, seek what is available for maps
-          rewards <- Gen.sequence[List[Reward],Reward]
-            { List.fill (as.size) (genReward) }
-          ars = as zip rewards
-        } yield Map (ars: _*)
-
-      val fs = agent.instances.allFiniteStates
-      val genStateActionRewards: Gen[Q] =
-        for  {
-          // TODO refactor, seek what is available for maps
-          mars <- Gen.sequence[List[Map[Action,Reward]], Map[Action,Reward]]
-            { List.fill (fs.size) (genActionReward) }
-          smars = fs zip mars
-        } yield Map (smars: _*)
-
-      genStateActionRewards
-    }
+    genStateActionRewards
+  }
 
   def pp_Q (q: Q): Doc = {
     def fmt (ar: Map[Action, Reward]): Doc =
