909213f848a80faf7e7dfbe00feb3c73e09cbf8f
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-04-27 20:20:33 +0200

    Maze: move to indentation of 3 steps
    This should make things easier to compile for Einar

--- src/main/scala/symsim/examples/concrete/simplemaze/Maze.scala
+++ src/main/scala/symsim/examples/concrete/simplemaze/Maze.scala
@@ -20,7 +20,7 @@ import symsim.concrete.Randomized
  */
 
 case class MazeState (x: Int, y: Int):
-  override def toString: String = s"[x=$x, y=$y]"
+   override def toString: String = s"[x=$x, y=$y]"
 
 type MazeFiniteState = MazeState
 type MazeReward = Double
@@ -33,53 +33,52 @@ case object Down extends MazeAction
 
 
 object Maze
-  extends Agent[MazeState, MazeFiniteState, MazeAction, MazeReward, Randomized]:
+   extends Agent[MazeState, MazeFiniteState, MazeAction, MazeReward, Randomized]:
 
-    def isFinal (s: MazeState): Boolean =
-      s == MazeState (4, 3) || s == MazeState (4, 2)
+      def isFinal (s: MazeState): Boolean =
+         s == MazeState (4, 3) || s == MazeState (4, 2)
 
-    // Maze is discrete
-    def discretize (s: MazeState): MazeFiniteState =  s
+      // Maze is discrete
+      def discretize (s: MazeState): MazeFiniteState =  s
 
-    private def mazeReward (s: MazeState): MazeReward = s match {
-      case MazeState (4, 3) => 10.0   // Good final state
-      case MazeState (4, 2) => -10.0  // Bad final state
-      case MazeState (_, _) => -1.0
-    }
-
-
-    def distort (a: MazeAction): Randomized[MazeAction] = a match
-      case Up | Down => Randomized.oneOf (Left, Right)
-      case Left | Right => Randomized.oneOf (Up, Down)
+      private def mazeReward (s: MazeState): MazeReward = s match
+        case MazeState (4, 3) => 10.0   // Good final state
+        case MazeState (4, 2) => -10.0  // Bad final state
+        case MazeState (_, _) => -1.0
 
 
-    def successor (s: MazeState) (a: MazeAction): MazeState =
-      require (valid (s))
-      val result = a match
-        case Up => s.copy (y = s.y+1)
-        case Down => s.copy (y = s.y-1)
-        case Left => s.copy (x = s.x-1)
-        case Right => s.copy (x = s.x+1)
-      if valid (result) then result else s
+      def distort (a: MazeAction): Randomized[MazeAction] = a match
+         case Up | Down => Randomized.oneOf (Left, Right)
+         case Left | Right => Randomized.oneOf (Up, Down)
 
-    def valid (s: MazeState): Boolean =
-      s.x >= 1 && s.x <= 4 && s.y >= 1 && s.y <= 3 && s != MazeState (2, 2)
 
-    val attention = 0.8
+      def successor (s: MazeState) (a: MazeAction): MazeState =
+         require (valid (s))
+         val result = a match
+            case Up => s.copy (y = s.y+1)
+            case Down => s.copy (y = s.y-1)
+            case Left => s.copy (x = s.x-1)
+            case Right => s.copy (x = s.x+1)
+         if valid (result) then result else s
 
-    def step (s: MazeState) (a: MazeAction): Randomized[(MazeState, MazeReward)] =
-      for
-        precise <- Randomized.coin (attention)
-        action <- if precise then Randomized.const (a) else distort (a)
-        newState = successor (s) (action)
-      yield (newState, mazeReward (newState))
+      def valid (s: MazeState): Boolean =
+         s.x >= 1 && s.x <= 4 && s.y >= 1 && s.y <= 3 && s != MazeState (2, 2)
 
-    def initialize: Randomized[MazeState] =
-      Randomized.oneOf (instances.allFiniteStates.filter { s => !isFinal (s) }:_*)
+      val attention = 0.8
 
-    override def zeroReward: MazeReward = 0
+      def step (s: MazeState) (a: MazeAction): Randomized[(MazeState, MazeReward)] =
+         for
+            precise <- Randomized.coin (attention)
+            action <- if precise then Randomized.const (a) else distort (a)
+            newState = successor (s) (action)
+         yield (newState, mazeReward (newState))
 
-    val instances = MazeInstances
+      def initialize: Randomized[MazeState] =
+         Randomized.oneOf (instances.allFiniteStates.filter { s => !isFinal (s) }:_*)
+
+      override def zeroReward: MazeReward = 0
+
+      val instances = MazeInstances
 
 
 
@@ -87,36 +86,36 @@ object Maze
   * needs to be able to do to work in the framework.
   */
 object MazeInstances
-  extends AgentConstraints[MazeState, MazeFiniteState, MazeAction, MazeReward, Randomized]:
+   extends AgentConstraints[MazeState, MazeFiniteState, MazeAction, MazeReward, Randomized]:
 
-  given enumAction: BoundedEnumerable[MazeAction] =
-    BoundedEnumerableFromList (Up, Down, Left, Right)
+   given enumAction: BoundedEnumerable[MazeAction] =
+      BoundedEnumerableFromList (Up, Down, Left, Right)
 
-  given enumState: BoundedEnumerable[MazeFiniteState] =
-    val ss = for
-      y <- List (1, 2, 3)
-      x <- List (1, 2, 3, 4)
-      result = MazeState (x, y)
-      if Maze.valid (result)
-    yield result
-    BoundedEnumerableFromList (ss: _*)
+   given enumState: BoundedEnumerable[MazeFiniteState] =
+      val ss = for
+         y <- List (1, 2, 3)
+         x <- List (1, 2, 3, 4)
+         result = MazeState (x, y)
+         if Maze.valid (result)
+      yield result
+      BoundedEnumerableFromList (ss: _*)
 
-  given schedulerIsMonad: Monad[Randomized] =
-    concrete.Randomized.randomizedIsMonad
+   given schedulerIsMonad: Monad[Randomized] =
+      concrete.Randomized.randomizedIsMonad
 
-  given canTestInScheduler: CanTestIn[Randomized] =
-    concrete.Randomized.canTestInRandomized
+   given canTestInScheduler: CanTestIn[Randomized] =
+      concrete.Randomized.canTestInRandomized
 
-  lazy val genMazeState: Gen[MazeState] = for
-    y <- Gen.choose (1,3)
-    x <- Gen.choose (1,4)
-    if (x != 2 && y != 2)
-  yield MazeState (x = x.abs, y = y.abs)
+   lazy val genMazeState: Gen[MazeState] = for
+      y <- Gen.choose (1,3)
+      x <- Gen.choose (1,4)
+      if (x != 2 && y != 2)
+   yield MazeState (x = x.abs, y = y.abs)
 
-  given arbitraryState: Arbitrary[MazeState] = Arbitrary (genMazeState)
+   given arbitraryState: Arbitrary[MazeState] = Arbitrary (genMazeState)
 
-  given eqMazeState: Eq[MazeState] = Eq.fromUniversalEquals
+   given eqMazeState: Eq[MazeState] = Eq.fromUniversalEquals
 
-  given arbitraryReward: Arbitrary[MazeReward] = Arbitrary (Gen.double)
+   given arbitraryReward: Arbitrary[MazeReward] = Arbitrary (Gen.double)
 
-  given rewardArith: Arith[MazeReward] = Arith.given_Arith_Double
+   given rewardArith: Arith[MazeReward] = Arith.given_Arith_Double
