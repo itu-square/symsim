a6d727c981a78e0535916ff677fb29089397d0b5
Mahsa Varshosaz <mahv@itu.dk>
2021-05-04 14:36:29 +0200

    updating experiments

--- src/test/scala/symsim/examples/concrete/windygrid/Experiments.scala
+++ src/test/scala/symsim/examples/concrete/windygrid/Experiments.scala
@@ -10,7 +10,7 @@ class Experiments
   with org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
   with org.scalatest.prop.TableDrivenPropertyChecks {
 
-  "test run" ignore {
+  "test run" in {
 
     // Import evidence that states and actions can be enumerated
     import WindyGrid._
@@ -23,12 +23,17 @@ class Experiments
       agent = WindyGrid,
       alpha = 0.1,
       gamma = 0.1,
-      distraction = 0.05, // explore vs exploit ration.
-      epochs = 5000,
+      epsilon = 0.1, // explore vs exploit ration.
+      epochs = 100000,
       seed = 1000
     )
 
-    val policy = sarsa.run
+    val q = sarsa.runQ
+    val policy = sarsa.qToPolicy (q)
+    val policy_output = sarsa.pp_policy (policy).hang (4)
+    info (policy_output.render (80))
+    val q_output = sarsa.pp_Q (q).hang (4)
+    info (q_output.render (80))
 
   }
 
