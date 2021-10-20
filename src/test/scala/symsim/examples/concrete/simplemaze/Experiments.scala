10092cfbc5e9e323dac4f63e6152fb6fa16249f9
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-10-20 11:37:57 +0200

    simplemaze.Experiments: Clean-up a bit and ignore by default

--- src/test/scala/symsim/examples/concrete/simplemaze/Experiments.scala
+++ src/test/scala/symsim/examples/concrete/simplemaze/Experiments.scala
@@ -1,34 +1,25 @@
 package symsim
 package examples.concrete.simplemaze
 
-import symsim.concrete.ConcreteSarsa
-import org.typelevel.paiges.Doc
+class Experiments
+   extends org.scalatest.freespec.AnyFreeSpec:
 
-class SimpleMazeExperiments
-  extends org.scalatest.freespec.AnyFreeSpec
-  with org.scalatest.matchers.should.Matchers
-  with org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-  with org.scalatest.prop.TableDrivenPropertyChecks:
+   "SimpleMaze test run" ignore {
 
-  "test run" in {
+      val sarsa = symsim.concrete.ConcreteSarsa (
+        agent = Maze,
+        alpha = 0.1,
+        gamma = 1.0,
+        epsilon = 0.05,
+        episodes = 250000,
+      )
 
-    val sarsa = ConcreteSarsa[
-      MazeState,
-      MazeFiniteState,
-      MazeAction
-    ] (
-      agent = Maze,
-      alpha = 0.1,
-      gamma = 1.0,
-      epsilon = 0.05, // explore vs exploit ratio
-      episodes = 100000,
-    )
+      val q = sarsa.runQ
+      val policy = sarsa.qToPolicy (q)
+      val policy_output = sarsa.pp_policy (policy).hang (4)
+      info (policy_output.render (80))
+      val q_output = sarsa.pp_Q (q).hang (4)
+      info (q_output.render (80))
+   }
 
-    val q = sarsa.runQ
-    val policy = sarsa.qToPolicy (q)
-    val policy_output = sarsa.pp_policy (policy).hang (4)
-    info (policy_output.render (80))
-    val q_output = sarsa.pp_Q (q).hang (4)
-    info (q_output.render (80))
-
-  }
+end Experiments
