dffb693fe7a2797ce5fb4411b68a1a61ca1fcfd7
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-11-09 21:55:51 +0100

    concrete.breaking.CarSpec: Fix tests to use only legal actions
    - Also refactor tests to use shared generators
    - Also refactor tests to follow indentation and naming conventions

--- src/test/scala/symsim/examples/concrete/breaking/CarSpec.scala
+++ src/test/scala/symsim/examples/concrete/breaking/CarSpec.scala
@@ -20,66 +20,44 @@ class CarSpec
 
    "Sanity checks for symsim.concrete.breaking" - {
 
+       // Generators of test data
        val positions = Gen.choose[Double] (0.0, 10.0)
+       val velocities = Gen.choose[Double] (0.0,10.0)
+       val actions = Gen.oneOf (Car.instances.enumAction.membersAscending)
+
+       // Tests
 
        "The car cannot move backwards" in check {
-          forAll (positions) { p =>
-             forAll { (a: Double) =>
-               val (s1, r) = Car.step (CarState (v = 0.0, p = p)) (a).head
-               (a <= 0) ==> (s1.p >= p)
-             }
+          forAll (positions, actions) { (p, a) =>
+             val (s1, r) = Car.step (CarState (v = 0.0, p = p)) (a).head
+             (a <= 0) ==> (s1.p >= p)
           }
        }
 
-       "The car cannot move backwards (regression)" in {
-          val p = 1.0
-          val a = 0.0
-          val (s1, r) = Car.step (CarState (v = 0.0, p = p)) (a).head
-          assert (s1.p >= p)
-       }
-
-       val gen01 = Gen.choose[Double] (0.0, 1.0)
-       val decels = Gen.choose[Double] (-10.0, -5.0)
 
        "Position never becomes negative" in check {
-          forAll (gen01, positions, decels) { (v, p, a) =>
-             val (s1, r) = Car.step (CarState (v, p)) (a).head
+          forAll (velocities, positions, actions) { (v, p, a) =>
+             val (s1, r) = Car.step (CarState (v = v, p = p)) (a).head
              s1.p >= 0.0
           }
        }
 
-       val Velocity = Gen.choose[Double] (0.0,10.0)
-
-       val Deceleration = Gen.choose[Double] (-10.0,0.0)
-
-         val Pairs : Gen[(Double, Double)] = for {
-         p1 <- Gen.choose[Double] (0.0,10.0)
-         p2 <- Gen.choose[Double] (0.0,10.0)
-       } yield (p1, p2)
 
        "Reward is valid 1" in check {
-         forAll  (Pairs) { posPair=>
-           forAll (Velocity){v=>
-             forAll (Deceleration) { a=>
-               val r1 = Car.step(CarState(v,posPair._1))(a).head._2
-               val r2 = Car.step(CarState(v,posPair._2))(a).head._2
-               posPair._1 <= posPair._2 ==> r1 >= r2
+          forAll  (positions, positions, velocities, actions) { (p1, p2, v, a) =>
+             val r1 = Car.step (CarState (v = v, p = p1)) (a).head._2
+             val r2 = Car.step (CarState (v = v, p = p2)) (a).head._2
+             p1 <= p2 ==> r1 >= r2
           }
-         }
-        }
        }
-// TODO:        arg0 = (1.5114558155620142E-5,1.821453015080769E-5), // 36 shrinks
-// [info]       arg1 = 0.0, // 19 shrinks
-// [info]       arg2 = -1.8141283195730265E-5 // 19 shrinks
+
+
        "Reward is valid 2" in check {
-         forAll  (Pairs) { velPair=>
-           forAll (positions){p=>
-             forAll (Deceleration) { a=>
-               val r1 = Car.step(CarState(velPair._1,p))(a).head._2
-               val r2 = Car.step(CarState(velPair._2,p))(a).head._2
-               velPair._1 <= velPair._2 ==> r1 >= r2
+          forAll  (velocities, velocities, positions, actions) { (v1, v2, p, a) =>
+             val r1 = Car.step (CarState (v1, p)) (a).head._2
+             val r2 = Car.step (CarState (v2, p)) (a).head._2
+             v1 <= v2 ==> r1 >= r2
           }
-         }
-        }
        }
+
     }
