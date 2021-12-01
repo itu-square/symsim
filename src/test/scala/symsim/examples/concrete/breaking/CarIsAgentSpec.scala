408698e559ac3e139b80681c487c01afd1c7d2bd
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2021-12-01 12:51:49 +0100

    MountainCar: Make Episodic, but without test for now

--- src/test/scala/symsim/examples/concrete/breaking/CarIsAgentSpec.scala
+++ src/test/scala/symsim/examples/concrete/breaking/CarIsAgentSpec.scala
@@ -4,6 +4,7 @@ package examples.concrete.breaking
 import laws.AgentLaws
 import laws.EpisodicLaws
 
-class CarIsAgentSpec extends SymSimSpec:
+class CarIsAgentSpec
+   extends SymSimSpec:
    checkAll ("concrete.breaking.Car is an Agent", AgentLaws (Car).laws)
    checkAll ("concrete.breaking.Car is Episodic", EpisodicLaws (Car).laws)
--- src/test/scala/symsim/examples/concrete/mountaincar/MountainCarIsAgentSpec.scala
+++ src/test/scala/symsim/examples/concrete/mountaincar/MountainCarIsAgentSpec.scala
@@ -2,7 +2,12 @@ package symsim
 package examples.concrete.mountaincar
 
 import laws.AgentLaws
+import laws.EpisodicLaws
 
 class MountainCarIsAgentSpec
    extends SymSimSpec:
    checkAll ("concrete.mountaincar.MountainCar is an Agent", AgentLaws (MountainCar).laws)
+   // Randomized testing of this law is too simplistic for mountaincar to
+   // reliably pass the test, so it is deactivated for now. A symbolic test'
+   // could be more efficient.
+   // checkAll ("concrete.mountaincar.MountainCar is Episodic", EpisodicLaws (MountainCar).laws)
