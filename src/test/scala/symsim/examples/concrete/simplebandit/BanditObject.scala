f04c1f8d97f36dccc4b67258376325955b763b91
Mohsen Ghaffari <77638469+mohsen-ghaffari1992@users.noreply.github.com>
2022-01-18 15:22:44 +0100

    Create BanditObject.scala

---
+++ src/test/scala/symsim/examples/concrete/simplebandit/BanditObject.scala
@@ -0,0 +1,15 @@
+package symsim
+package examples.concrete.simplebandit
+
+import symsim.concrete.Randomized
+
+val BanditObj = new Bandit(List(Randomized.gaussian(-0.2, 1),
+		Randomized.gaussian(0.2, 1),
+		Randomized.gaussian(0.4, 1),
+		Randomized.gaussian(-0.1, 1),
+		Randomized.gaussian(0.3, 1),
+		Randomized.gaussian(0.8, 1),
+		Randomized.gaussian(0.6, 1),
+		Randomized.gaussian(-0.6, 1),
+		Randomized.gaussian(0.0, 1),
+		Randomized.gaussian(0.7, 1)))
