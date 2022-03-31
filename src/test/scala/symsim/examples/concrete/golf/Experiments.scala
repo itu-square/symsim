54fc59ef51698e9f721ea855db8e1a176dd4af83
Mohsen Ghaffari <77638469+mohsen-ghaffari1992@users.noreply.github.com>
2022-03-31 13:28:46 +0200

    Create Experiments.scala

---
+++ src/test/scala/symsim/examples/concrete/golf/Experiments.scala
@@ -0,0 +1,16 @@
+package symsim
+package examples.concrete.golf
+
+class Experiments
+   extends ExperimentSpec[GolfState,GolfState,GolfAction]:
+   import Golf._
+
+   val sarsa = symsim.concrete.ConcreteSarsa (
+     agent = Golf,
+     alpha = 0.1,
+     gamma = 0.1,
+     epsilon = 0.1,
+     episodes = 20000,
+   )
+
+   s"Golf experiment with ${sarsa}" in { val policy = learnAndLog (sarsa) }
