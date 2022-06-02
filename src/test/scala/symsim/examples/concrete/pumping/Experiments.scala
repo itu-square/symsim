0294a1c0a5d525596ef6fba978b2d5f1c757ca4f
Mohsen Ghaffari <77638469+mohsen-ghaffari1992@users.noreply.github.com>
2022-06-02 17:55:00 +0200

    Create Experiments.scala

---
+++ src/test/scala/symsim/examples/concrete/pumping/Experiments.scala
@@ -0,0 +1,20 @@
+package symsim
+package examples.concrete.pumping
+
+class Experiments
+   extends ExperimentSpec[PumpState,PumpFiniteState,PumpAction]:
+
+   // Import evidence that states and actions can be enumerated
+   import Pump._
+
+   val sarsa = symsim.concrete.ConcreteSarsa (
+      agent = Pump,
+      alpha = 0.1,
+      gamma = 0.1,
+      epsilon = 0.05,
+      episodes = 1,
+   )
+
+   s"Pumping experiment with $sarsa" in {
+     val policy = learnAndLog (sarsa)
+   }
