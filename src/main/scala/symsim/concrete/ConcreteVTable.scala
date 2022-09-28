9f4fd00d31c165ed07913b51602cf059e35e8fca
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2022-09-28 16:36:41 +0200

    ConcreteVTable: Remove the best_action function temporarily

---
+++ src/main/scala/symsim/concrete/ConcreteVTable.scala
@@ -0,0 +1,22 @@
+package symsim
+package concrete
+
+trait ConcreteVTable[State, ObservableState, Action]
+  extends VTable[State, ObservableState, Action, Double, Randomized]:
+
+  this: ConcreteExactRL[State, ObservableState, Action] =>
+
+  import agent.instances.*
+
+  type Scheduler[A] = Randomized[A]
+
+  /** TODO: is this function used by VTable algorithms at all? 
+    * I have replaced it by unimplemented for now, for safety. */
+  def bestAction (v: V) (s: State): Action = ???
+    
+  def chooseAction (v: V) (s: State): Scheduler[Action] = for
+    explore <- Randomized.coin (this.epsilon)
+    action <- if explore
+    then Randomized.oneOf (allActions*)
+    else Randomized.const (bestAction (v) (s))
+  yield action
