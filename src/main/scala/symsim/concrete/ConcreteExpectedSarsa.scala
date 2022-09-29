3d824da1ee34c334bb07258efbec30d09f841800
mohsen-ghaffari1992 <mohg@itu.dk>
2022-09-29 12:41:50 +0200

    Expected Sarsa Algorithm

--- src/main/scala/symsim/concrete/ConcreteExpectedSarsa.scala
+++ src/main/scala/symsim/concrete/ConcreteExpectedSarsa.scala
@@ -1,14 +1,15 @@
 package symsim
 package concrete
 
-case class ConcreteExpectedSarsa[State, FiniteState, Action] (
-     val agent: Agent[State, FiniteState, Action, Double, Randomized],
-     val alpha: Double,
-     val gamma: Double,
-     val epsilon: Probability,
-     val episodes: Int,
-) extends ExpectedSarsa[State, FiniteState, Action, Double, Randomized]
-    with ConcreteExactRL[State, FiniteState, Action]:
+case class ConcreteExpectedSarsa[State, ObservableState, Action] (
+ val agent: Agent[State, ObservableState, Action, Double, Randomized],
+ val alpha: Double,
+ val gamma: Double,
+ val epsilon: Probability,
+ val episodes: Int,
+) extends ExpectedSarsa[State, ObservableState, Action, Double, Randomized],
+  ConcreteExactRL[State, ObservableState, Action],
+  ConcreteQTable[State, ObservableState, Action]:
 
-    override def toString: String =
-        s"ExpectedSarsa-Learn(α=$alpha, 𝛾=$gamma, 𝜀=$epsilon, $episodes episodes)"
+  override def toString: String =
+    s"ExpectedSarsa-Learn(α=$alpha, 𝛾=$gamma, 𝜀=$epsilon, $episodes episodes)"
