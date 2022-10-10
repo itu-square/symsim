e2e976cb870ff4e14baa29fe14d5aac805bb3b05
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2022-10-10 11:11:07 +0200

    testing: Make outputing the QTable and Policy optional
    + fix whitespace issues

--- src/test/scala/symsim/ExperimentSpec.scala
+++ src/test/scala/symsim/ExperimentSpec.scala
@@ -1,16 +1,20 @@
 package symsim
 
 import symsim.concrete.ConcreteQTable
+
 trait ExperimentSpec[State, ObservableState, Action]
-   extends org.scalatest.freespec.AnyFreeSpec
-   with org.scalatest.matchers.should.Matchers:
+  extends org.scalatest.freespec.AnyFreeSpec,
+  org.scalatest.matchers.should.Matchers:
 
    def learnAndLog (setup: concrete.ConcreteExactRL[State, ObservableState, Action]
-           with ConcreteQTable[State, ObservableState, Action]) =
-      val q = setup.runQ
-      val policy = setup.qToPolicy (q)
-      val policy_output = setup.pp_policy (policy).hang (4)
-      info (policy_output.render (80))
-      val q_output = setup.pp_Q (q).hang (4)
-      info (q_output.render (80))
-      policy
+     with ConcreteQTable[State, ObservableState, Action],
+     outputQTable: Boolean = true,
+     outputPolicy: Boolean = true) =
+
+     val q = setup.runQ
+     val policy = setup.qToPolicy (q)
+     val policy_output = setup.pp_policy (policy).hang (4)
+     if outputQTable then info (policy_output.render (80))
+     val q_output = setup.pp_Q (q).hang (4)
+     if outputPolicy then info (q_output.render (80))
+     policy
