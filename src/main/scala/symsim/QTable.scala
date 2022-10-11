42d9b419fd2f89ac66ce1259e750c5272b2ed617
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2022-10-11 19:02:02 +0200

    QTable: Refactor genVF for clarity and conciseness

--- src/main/scala/symsim/QTable.scala
+++ src/main/scala/symsim/QTable.scala
@@ -6,6 +6,7 @@ import cats.syntax.flatMap.*
 import cats.syntax.foldable.*
 import cats.syntax.option.*
 import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
 import org.typelevel.paiges.Doc
 import symsim.concrete.{ConcreteExactRL, Randomized}
 
@@ -13,7 +14,9 @@ import symsim.concrete.{ConcreteExactRL, Randomized}
 trait QTable[State, ObservableState, Action, Reward, Scheduler[_]]
   extends ValueFunction[State, ObservableState, Action, Reward, Scheduler]:
   this: ExactRL[State, ObservableState, Action, Reward, Scheduler] =>
+
   import agent.instances.*
+  import agent.instances.given
 
   type Q = Map[ObservableState, Map[Action, Reward]]
   type VF = Q
@@ -33,21 +36,18 @@ trait QTable[State, ObservableState, Action, Reward, Scheduler[_]]
     q0
 
 
-  /** Generate total Q matrices for testing. */
+  /** Generate total Q matrices for testing. 
+    *
+    * Note that just asking for arbitrary[Map[ObservableState, Map[Action,
+    * Reward]]] will return rnaomd Q-tables, but not guaranteeing that they are
+    * total (for instance, by default an empty Map will be produced). 
+    **/
   val genVF: Gen[Q] =
-    val as = agent.instances.allActions
-    val genReward = agent.instances.arbitraryReward.arbitrary
-    val genActionReward: Gen[Map[Action, Reward]] = for
-      rewards <- Gen.sequence[List[Reward], Reward] {
-        List.fill (as.size) (genReward) }
-      ars = as zip rewards
-    yield Map (ars*)
+    val l1 = List.fill (allActions.size) (arbitrary[Reward]) 
+    val genActionRewardMap = 
+      for rs <- Gen.sequence[List[Reward], Reward] (l1)
+      yield Map (allActions zip rs*)
 
-    val fs = agent.instances.allObservableStates
-    val genStateActionRewards: Gen[Q] = for
-      mars <- Gen.sequence[List[Map[Action, Reward]],
-        Map[Action, Reward]] { List.fill (fs.size) (genActionReward) }
-      smars = fs zip mars
-    yield Map (smars*)
-
-    genStateActionRewards
+    val  l2 = List.fill (allObservableStates.size) (genActionRewardMap)
+    for maps <- Gen.sequence[List[Map[Action, Reward]], Map[Action, Reward]] (l2)
+    yield Map (allObservableStates zip maps*)
