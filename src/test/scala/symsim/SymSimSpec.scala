56223826a7b54971fb64b3275e508c782f54b8f3
mohsen-ghaffari1992 <mohg@itu.dk>
2022-10-31 07:57:15 +0100

    Fix the number of tests. issue #150

--- src/test/scala/symsim/SymSimSpec.scala
+++ src/test/scala/symsim/SymSimSpec.scala
@@ -2,7 +2,7 @@ package symsim
 
 trait SymSimSpec
   extends org.typelevel.discipline.scalatest.FunSuiteDiscipline,
-          org.scalatest.funsuite.AnyFunSuiteLike,
-          org.scalatest.prop.Configuration:
+    org.scalatest.funsuite.AnyFunSuiteLike,
+    org.scalatest.prop.Configuration:
     given PropertyCheckConfiguration =
         PropertyCheckConfiguration(minSuccessful = 100)
