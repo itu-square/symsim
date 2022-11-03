291b3eb426a45c0ad649fed001a53a1594abc352
mohsen-ghaffari1992 <mohg@itu.dk>
2022-11-03 14:46:30 +0100

    intendent in MountainCarIsAgentSpec and new line in PumpIsAgentSpec are solved.

--- src/test/scala/symsim/examples/concrete/mountaincar/MountainCarIsAgentSpec.scala
+++ src/test/scala/symsim/examples/concrete/mountaincar/MountainCarIsAgentSpec.scala
@@ -8,7 +8,7 @@ class MountainCarIsAgentSpec
   extends SymSimSpec:
 
   checkAll ("concrete.mountaincar.MountainCar is an Agent", AgentLaws (MountainCar).laws)
-// Randomized testing of this law is too simplistic for mountaincar to
-// reliably pass the test, so it is deactivated for now. A symbolic test'
-// could be more efficient.
-// checkAll ("concrete.mountaincar.MountainCar is Episodic", EpisodicLaws (MountainCar).laws)
+  // Randomized testing of this law is too simplistic for mountaincar to
+  // reliably pass the test, so it is deactivated for now. A symbolic test'
+  // could be more efficient.
+  // checkAll ("concrete.mountaincar.MountainCar is Episodic", EpisodicLaws (MountainCar).laws)
