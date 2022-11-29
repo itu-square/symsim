df31ec0ba34a855789e2b904a13397e11236ea0f
mohsen-ghaffari1992 <mohg@itu.dk>
2022-11-29 14:15:06 +0100

    Add spec tests for mountain car example

--- src/test/scala/symsim/examples/concrete/mountaincar/MountainCarSpec.scala
+++ src/test/scala/symsim/examples/concrete/mountaincar/MountainCarSpec.scala
@@ -5,10 +5,7 @@ import symsim.concrete.Randomized.given
 import CanTestIn.given
 import org.scalatest.*
 import prop.*
-import org.scalacheck.Arbitrary.*
 import org.scalacheck.Gen
-import org.scalatest.prop.Whenever
-import org.scalatest.*
 import org.scalacheck.Prop.{exists, forAll, forAllNoShrink, propBoolean}
 import examples.concrete.mountaincar.CarState
 import examples.concrete.mountaincar.MountainCar
@@ -22,7 +19,7 @@ class MountainCarSpec
   given PropertyCheckConfiguration =
     PropertyCheckConfiguration(minSuccessful = 100)
 
-  "Sanity checks for symsim.concrete.breaking" - {
+  "Sanity checks for symsim.concrete.mountaincar" - {
 
     // Generators of test data
     val positions = Gen.choose[Double] (-1.2, 0.5)
@@ -33,32 +30,38 @@ class MountainCarSpec
 
     // Tests
 
-    "Achieving goal state from state (0.0, 0.0) is impossible" in check {
+    "Achieving the goal state from state (0.0, 0.0) with a single action is impossible" in check {
       forAll (actions) { a =>
         for (s1, r) <- MountainCar.step (CarState (0.0, 0.0)) (a)
-        yield s1.p != 0.5
+        yield s1.p < 0.5
       }
     }
 
-    "there is a left bound for position" in check {
+    "The car cannot move beyond -1.2 on the left" in check {
       val dt: Double = 0.1
       val mass: Double = 0.2
       val friction: Double = 0.3
       val gravity: Double = 9.8
-      forAllNoShrink (velocities, negPos, actions) { (v, p, a) =>
-        val v1 = v + (gravity * mass * cos(3.0 * p) + a / mass - friction * v) * dt
+      forAll (velocities, negPos, actions) { (v, p, a) =>
+        val v1 = v + (-gravity * mass * cos(3.0 * p) + a / mass - friction * v) * dt
         val p1 = p + (v1 * dt)
         for (s1, r) <- MountainCar.step (CarState (v, p)) (a)
           yield (p1 < -1.2) ==> (s1.p == -1.2)
       }
     }
 
-    "moving to the left bound of position is possible" in check {
-      forAllNoShrink (velocities, actions) { (v, a) =>
-        exists (negPos) { p =>
-          for (s1, r) <- MountainCar.step (CarState (v, p)) (a)
-          yield s1.p == -1.2
-        }
+    "The car can move to the position -1.2" in check {
+      val (s1, r) = MountainCar.step (
+        CarState (-1.1601645563843357, -1.1533417842134712)) (-0.2).head
+      s1.p == -1.2
+    }
+
+    "gravity and friction are both decreasing the velocity" in check {
+      forAllNoShrink (velocities, negPos) { (v, p1) =>
+        for
+          (s1, r1) <- MountainCar.step(CarState(v, p1))(0)
+          (s2, r2) <- MountainCar.step(CarState(v, s1.p))(0)
+        yield ((p1 < s1.p) ==> (s1.v < s2.v)) || ((p1 > s1.p) ==> (s1.v > s2.v))
       }
     }
 
