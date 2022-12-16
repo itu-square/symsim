832b991bfb8b9f8b39356f0b915c2c9ac6d53af5
mohsen-ghaffari1992 <mohg@itu.dk>
2022-12-16 12:45:26 +0100

    Changing scalatest to scalacheck

--- src/test/scala/symsim/examples/concrete/windygrid/WindyGridSpec.scala
+++ src/test/scala/symsim/examples/concrete/windygrid/WindyGridSpec.scala
@@ -3,23 +3,21 @@ package examples.concrete.windygrid
 
 import symsim.concrete.Randomized.given
 import CanTestIn.given
-import org.scalatest.*
-import prop.*
-import org.scalacheck.Gen
-import org.scalacheck.Prop.{exists, forAll, forAllNoShrink, propBoolean}
+import org.scalacheck.{Arbitrary, Gen, Prop}
+import org.scalacheck.Arbitrary.*
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.*
 import examples.concrete.windygrid.GridState
 import examples.concrete.windygrid.WindyGrid
 import symsim.concrete.{ConcreteSarsa, Randomized}
 
+// To eliminate the warning on WindyGird, until scalacheck makes it open
+import scala.language.adhocExtensions
+
 /** Sanity tests for Randomized as a Scheduler */
 class WindyGridSpec
-  extends org.scalatest.freespec.AnyFreeSpec,
-    org.scalatestplus.scalacheck.Checkers:
+  extends org.scalacheck.Properties ("WindyGrid"):
 
-  given PropertyCheckConfiguration =
-    PropertyCheckConfiguration(minSuccessful = 100)
-
-  "Sanity checks for symsim.concrete.windygrid" - {
 
     // Generators of test data
     val xs = Gen.choose [Int](1, 10)
@@ -28,7 +26,7 @@ class WindyGridSpec
 
     // Tests
 
-    "Up and Down will never affect the x value" in check {
+    property ("Up and Down will never affect the x value") = {
       forAll (xs, ys) { (x, y) =>
         for
           (s1, r) <- WindyGrid.step (GridState (x, y)) (GridAction.U)
@@ -37,23 +35,23 @@ class WindyGridSpec
       }
     }
 
-    "When there is wind, R will affect both x and y unless in the y's upper bound" in check {
-      forAll(xs, ys) { (x, y) =>
+    property ("When there is wind, R will affect both x and y unless in the y's upper bound") = {
+      forAll (xs, ys) { (x, y) =>
         for
           (s1, r) <- WindyGrid.step (GridState (x, y)) (GridAction.R)
         yield (x >= 4 && x <= 8 && y <= 6) ==> (s1._1 != x && s1._2 != y)
       }
     }
 
-    "When there is wind, L will affect both x and y unless in the y's upper bound" in check {
-      forAll(xs, ys) { (x, y) =>
+    property ("When there is wind, L will affect both x and y unless in the y's upper bound") = {
+      forAll (xs, ys) { (x, y) =>
         for
           (s1, r) <- WindyGrid.step (GridState (x, y)) (GridAction.L)
         yield (x >= 4 && x <= 8 && y <= 6) ==> (s1._1 != x && s1._2 != y)
       }
     }
 
-    "Q-table values are non-positive" in check {
+    property ("Q-table values are non-positive") = {
       val sarsa = ConcreteSarsa(WindyGrid, 0.1, 1, 0.05, 100000)
       forAll (xs, ys, actions) { (x, y, a) =>
         for
@@ -61,4 +59,3 @@ class WindyGridSpec
         yield Q (GridState(x, y)) (a) <= 0
       }
     }
-  }
