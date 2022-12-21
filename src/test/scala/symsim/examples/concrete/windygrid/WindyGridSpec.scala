d444222bdb87fa9c375b0dc780b4eff26ca734ff
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2022-12-21 15:41:43 +0100

    windygrid: Minor cleanups in tests

--- src/test/scala/symsim/examples/concrete/windygrid/WindyGridSpec.scala
+++ src/test/scala/symsim/examples/concrete/windygrid/WindyGridSpec.scala
@@ -17,15 +17,16 @@ class WindyGridSpec
   import WindyGrid.instances.{arbitraryAction, arbitraryState}
 
   property ("Up and Down will never affect the x value") =
-    forAll { (s: GridState) => for
+    forAll { (s: GridState) => 
+      for
         (s1, r) <- WindyGrid.step (s) (GridAction.U)
         (s2, r) <- WindyGrid.step (s) (GridAction.D)
       yield s1._1 == s.x && s2._1 == s.x
     }
 
   property ("If wind, R affects both x and y unless in the y's upper bound") =
-    forAll { (s: GridState) => for 
-      (s1, r) <- WindyGrid.step (s) (GridAction.R)
+    forAll { (s: GridState) => 
+      for (s1, r) <- WindyGrid.step (s) (GridAction.R)
       yield (s.x >= 4 && s.x <= 8 && s.y <= 6) ==> (s1._1 != s.x && s1._2 != s.y)
     }
 
@@ -35,8 +36,10 @@ class WindyGridSpec
       yield (s.x >= 4 && s.x <= 8 && s.y <= 6) ==> (s1._1 != s.x && s1._2 != s.y)
     }
 
+  // The test runs 1 episode regardless the last argument value (`episodes`)
+  val sarsa = ConcreteSarsa (WindyGrid, 0.1, 0.5, 0.1, 1)
+
   property ("Q-table values are non-positive") =
-    val sarsa = ConcreteSarsa (WindyGrid, 0.1, 0.5, 0.1, 100)
     forAll { (s: GridState, a: GridAction) =>
       for Q <- sarsa.learningEpisode (sarsa.initialize, s)
       yield Q (s, a) <= 0
