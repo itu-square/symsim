825ee5cf3ee7aa847d974e5441094d64b6e2bf24
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2023-01-04 19:46:24 +0100

    temporary snapshot to move PCs

--- src/main/scala/symsim/BDL.scala
+++ src/main/scala/symsim/BDL.scala
@@ -42,24 +42,27 @@ case class BDLLearn[State, ObservableState, Action] (
 
   /** A single step of the learning algorithm
    *
-   *  @param q the last Q-matrix
+   *  @param q   the last Q-matrix
    *  @param s_t current state
    *  @return the updated matrix Q, the successor state, and a
    *          reward difference (the size of the update performed)
    */
   def learningEpoch (q: VF, s_t: State, a_t: Action)
     : Randomized[(VF, State, Action)] = ???
-     // for
-     //    sa_tt <- agent.step (s_t) (a_t)
-     //    (s_tt, r_tt) = sa_tt
-     //    // SARSA: on-policy (p.844 in Russel & Norvig)
-     //    a_tt <- chooseAction (q) (agent.observe (s_tt))
+    for
+      // sa_tt <- agent.step (s_t) (a_t)
+      // (s_tt, r_tt) = sa_tt
+      // // SARSA: on-policy (p.844 in Russel & Norvig)
+      // a_tt <- chooseAction (q) (agent.observe (s_tt))
 
-     //    ds_t = agent.observe (s_t)
-     //    ds_tt = agent.observe (s_tt)
-     //    old_entry = q (ds_t, a_t)
-     //    correction = r_tt + gamma * q (ds_tt, a_tt) - old_entry
-     //    qval = old_entry + alpha * correction
+      // ds_t = agent.observe (s_t)
+      // ds_tt = agent.observe (s_tt)
+      // old_entry = q (ds_t, a_t)
+      // correction = r_tt + gamma * q (ds_tt, a_tt) - old_entry
+      // qval = old_entry + alpha * correction
 
-     //    q1 = q.updated (ds_t, a_t, qval)
-     // yield (q1, s_tt, a_tt)
+      // q1 = q.updated (ds_t, a_t, qval)
+    yield (q1, s_tt, a_tt)
+
+
+    def est (est: List[Est]) (q: VF) (s_t: State, a_t: Action, 0?, 1?): ??? = ???
