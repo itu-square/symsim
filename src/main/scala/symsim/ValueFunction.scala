fe0958ecc1a972a5323b456a768d28c80f532972
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2023-02-08 14:04:27 +0100

    ValueFunction: Enrich the interface with getting action values

--- src/main/scala/symsim/ValueFunction.scala
+++ src/main/scala/symsim/ValueFunction.scala
@@ -1,9 +1,13 @@
 package symsim
 
 import org.scalacheck.Gen
+
 trait ValueFunction[ObservableState, Action, Reward, Scheduler[_]]:
   type VF
+  def value (vf: VF) (s: ObservableState, a: Action): Reward
   def bestAction (vf: VF) (s: ObservableState): Action
+  def bestActionValue (vf: VF) (s: ObservableState): Reward = 
+    value (vf) (s, bestAction (vf) (s))
   def chooseAction (vf: VF) (s: ObservableState): Scheduler[Action]
   def initialize: VF
   def genVF: Gen[VF]
