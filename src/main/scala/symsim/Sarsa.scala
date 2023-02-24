2ab25c949380693eedef589288e7c8771fd48180
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2023-02-24 19:55:21 +0100

    Sarsa: Rename ds_* states to os_* (consistent with the new terminology)
    + a few other renamings to be consistent with variable names in the bdl
      interpreter and in the paper

--- src/main/scala/symsim/Sarsa.scala
+++ src/main/scala/symsim/Sarsa.scala
@@ -21,19 +21,16 @@ trait Sarsa[State, ObservableState, Action, Reward, Scheduler[_]]
     * @return the updated matrix Q, the successor state, and a
     * reward difference (the size of the update performed)
     */
-  override def learningEpoch (q: VF, s_t: State, a_t: Action)
-    : Scheduler[(VF, State, Action)] =
-    for
-      sa_tt <- agent.step (s_t) (a_t)
-      (s_tt, r_tt) = sa_tt
-      // SARSA: on-policy (p.844 in Russel & Norvig)
-      a_tt <- vf.chooseAction (ε) (q) (agent.observe (s_tt))
-
-      ds_t = agent.observe (s_t)
-      ds_tt = agent.observe (s_tt)
-      old_entry = q (ds_t, a_t)
-      correction = r_tt + gamma * q (ds_tt, a_tt) - old_entry
-      qval = old_entry + alpha * correction
-
-      q1 = q.updated (ds_t, a_t, qval)
-    yield (q1, s_tt, a_tt)
+  override def learningEpoch (q_t: VF, s_t: State, a_t: Action)
+    : Scheduler[(VF, State, Action)] = for
+      sa_tt        <- agent.step (s_t) (a_t)
+      (s_tt, r_tt)  = sa_tt
+                      // SARSA: on-policy (p.844 in Russel & Norvig)
+      a_tt         <- vf.chooseAction (ε) (q_t) (agent.observe (s_tt))
+      os_t          = agent.observe (s_t)
+      os_tt         = agent.observe (s_tt)
+      old_entry     = q_t (os_t, a_t)
+      correction    = r_tt + gamma * q_t (os_tt, a_tt) - old_entry
+      qval          = old_entry + alpha * correction
+      q_tt          = q_t.updated (os_t, a_t, qval)
+    yield (q_tt, s_tt, a_tt)
