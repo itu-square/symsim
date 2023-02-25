71cdfc9e982bcdf4dd53908da0a5014d10758603
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2023-02-25 09:10:54 +0100

    ExpectedSarsa: Improve layout and uniformize names with current convention

--- src/main/scala/symsim/ExpectedSarsa.scala
+++ src/main/scala/symsim/ExpectedSarsa.scala
@@ -24,20 +24,19 @@ trait ExpectedSarsa[State, ObservableState, Action, Reward, Scheduler[_]]
   override def learningEpoch (q: Q, s_t: State, a_t: Action)
   : Scheduler[(Q, State, Action)] =
     for
-      sa_tt <- agent.step (s_t) (a_t)
-      (s_tt, r_tt) = sa_tt
-      // Expected Sarsa (p.133 in Sutton & Barto)
-      ds_t = agent.observe (s_t)
-      ds_tt = agent.observe (s_tt)
-      old_entry = q (ds_t, a_t)
+      sa_tt        <- agent.step (s_t) (a_t)
+      (s_tt, r_tt)  = sa_tt
+                    // Expected Sarsa (p.133 in Sutton & Barto)
+      (os_t, os_tt) = (agent.observe (s_t), agent.observe (s_tt))
+      old_entry     = q (os_t, a_t)
 
-      expectedQ = for
-        a_tt <- agent.instances.allActions
-        q_a_tt = (1.0 / agent.instances.allActions.length) .times[Reward] (q (ds_tt, a_tt))
-      yield q_a_tt
+      expectedQ     = for a_tt   <- agent.instances.allActions
+                          q_a_tt  = (1.0 / agent.instances.allActions.length) 
+                                      .times[Reward] (q (os_tt, a_tt))
+                      yield q_a_tt
 
-      correction = r_tt + gamma * expectedQ.foldLeft ((0.0).asInstanceOf[Reward])( _ + _ ) - old_entry
-      qval = old_entry + alpha * correction
-      q1 = q.updated (ds_t, a_t, qval)
-      a_tt1 <- vf.chooseAction (ε) (q1) (ds_tt)
+      correction    = r_tt + gamma * expectedQ.foldLeft ((0.0).asInstanceOf[Reward])( _ + _ ) - old_entry
+      qval          = old_entry + alpha * correction
+      q1            = q.updated (os_t, a_t, qval)
+      a_tt1        <- vf.chooseAction (ε) (q1) (os_tt)
     yield (q1, s_tt, a_tt1)
