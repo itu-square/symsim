bb95c1bbe83be93b17efdd3bd238fc4ca6ecb0ad
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2023-03-07 10:26:41 +0100

    Work in progress

--- src/main/scala/symsim/concrete/Stats.scala
+++ src/main/scala/symsim/concrete/Stats.scala
@@ -52,3 +52,39 @@ def GaussianMass (μ: Double, σ2: Double) (left: Double, right: Double)
   val mass_left = Gaussian (μ, σ2).cdf (left) 
   mass_right - mass_left
 
+
+/** Compute an HDI of mass p for density Beta(α, β).
+ *
+ *  This is up to some precision (because there is a numerical step in there,
+ *  bisection). 
+ *
+ *  Source: Andrey Akinshin. Trimmed Harrell-Davis quantile
+ *  estimator based on the highest density interval of the given width.  
+ *
+ *  whuber (https://stats.stackexchange.com/users/919/whuber), Credible set for
+ *  beta distribution, URL (version: 2015-07-05):
+ *  https://stats.stackexchange.com/q/160035
+ *
+ *  (but we do given probability mass not given width).
+ *
+ *  We only handle nondegenerate cases, with α and β bigger than one.
+ *  TODO: check if this is not provided by breeze already
+ *
+ *  @param α the number of successes observed (+1)
+ *  @param β the number of failures observed (+1)
+ *  @param p the probability mass of the sought interval
+ *  @param precision the precision of floating point equality for numerical
+ *                   search
+ *  @return an HDI of size `size` for density β(α, β).
+ */
+def betaHdi (α: Double, β: Double, p: Double, precision: Double = 1e-9): Double = 
+  require (α > 1.0 && β > 1.0)
+  val distr = Beta (α, β)
+  val mode = distr.mode
+  val l = distr.inverseCDF (p / 2.0)
+  val r = distr.inverseCDF (p / 2.0 + 1 - α)
+  // We need to find a zero in 2D space of this function
+  def f (l: Double, r: Double): Double = 
+    val diff = distr.pdf (l) - distr.pdf (r)
+    diff * diff
+  ???
