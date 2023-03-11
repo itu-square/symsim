a8a62392827b453622fe44cff25702c00e1e3c3b
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2023-03-11 15:20:10 +0100

    stats: Add a calculation of HDI/HDP/CI for Beta density

--- src/main/scala/symsim/concrete/Stats.scala
+++ src/main/scala/symsim/concrete/Stats.scala
@@ -52,39 +52,78 @@ def GaussianMass (μ: Double, σ2: Double) (left: Double, right: Double)
   val mass_left = Gaussian (μ, σ2).cdf (left) 
   mass_right - mass_left
 
+/** Numerically finds a minimum of the function f, using x0 as a seed
+ *  (starting point).
+ *
+ *  For the time being the gradient is approximated numerically,
+ *  although for our current use, we could probably find an analytical
+ *  solution. This would likely make the tests faster and less flaky.
+ */
+def minimize (f: Double => Double, x0: Double): Double =
+  import breeze.optimize.*
+  import breeze.linalg.DenseVector
+  import breeze.stats.distributions.*
+  
+  // I am wrapping it into vector of size 1, because I do not
+  // understand how to force breeze to optimize a function directly on
+  // a Double value.  This likely needs to be fixed one day
+  val differrentiableF = 
+    ApproximateGradientFunction { (x: DenseVector[Double]) => f(x(0)) }
 
-/** Compute an HDI of mass p for density Beta(α, β).
+  val lbfgs = new LBFGS[DenseVector[Double]] (maxIter = 100, m = 4) 
+  lbfgs.minimize(differrentiableF, DenseVector(x0)) (0)
+
+/** Compute an highest density interval of mass p for density Beta(α, β).
  *
- *  This is up to some precision (because there is a numerical step in there,
- *  bisection). 
+ *  This is approximate (because of a numerical optimization step)
+ *  Apparently, the procedure is quite general and we could use it for
+ *  other distributions too, but for the time being we are trying it
+ *  for Beta.
+ *  
+ *  We only handle nondegenerate cases, with α and β bigger than one.
  *
- *  Source: Andrey Akinshin. Trimmed Harrell-Davis quantile
- *  estimator based on the highest density interval of the given width.  
- *
- *  whuber (https://stats.stackexchange.com/users/919/whuber), Credible set for
- *  beta distribution, URL (version: 2015-07-05):
+ *  Source: whuber (https://stats.stackexchange.com/users/919/whuber),
+ *  Credible set for beta distribution, URL (version: 2015-07-05):
  *  https://stats.stackexchange.com/q/160035
  *
- *  (but we do given probability mass not given width).
+ *  Also: Andrey Akinshin. Trimmed Harrell-Davis quantile estimator
+ *  based on the highest density interval of the given width.  (but we
+ *  do given probability mass not given width).
  *
- *  We only handle nondegenerate cases, with α and β bigger than one.
- *  TODO: check if this is not provided by breeze already
  *
- *  @param α the number of successes observed (+1)
- *  @param β the number of failures observed (+1)
- *  @param p the probability mass of the sought interval
- *  @param precision the precision of floating point equality for numerical
- *                   search
- *  @return an HDI of size `size` for density β(α, β).
+ *  @param α the number of successes observed (+1), σ > 1
+ *  @param β the number of failures observed (+1), β > 1
+ *  @param p the probability mass of the sought credibility interval
+ *
+ *  @return the endpoints of the HDI of mass `p` for density β(α, β).
  */
-def betaHdi (α: Double, β: Double, p: Double, precision: Double = 1e-9): Double = 
-  require (α > 1.0 && β > 1.0)
+def betaHdi (α: Double, β: Double, p: Double): (Double, Double) = 
+
+  require (α > 1.0)
+  require (β > 1.0)
+  require (p >= 0.0)
+  require (p <= 1.0)
+
+  val pc = 1.0 - p
   val distr = Beta (α, β)
   val mode = distr.mode
-  val l = distr.inverseCDF (p / 2.0)
-  val r = distr.inverseCDF (p / 2.0 + 1 - α)
-  // We need to find a zero in 2D space of this function
-  def f (l: Double, r: Double): Double = 
-    val diff = distr.pdf (l) - distr.pdf (r)
-    diff * diff
-  ???
+  val l0 = distr.inverseCdf (pc / 2.0)
+
+  /** The objective function for the search.  We need to find a zero
+   *  in 2D space of this function.
+   */
+  def f (l: Double): Double = 
+    val pl = if l <= 0 then 0.0
+             else if l < 1.0 then distr.cdf (l)
+             else 1.0
+    val r = distr.inverseCdf ((pl+p) min 1.0)
+    val pr = if r <= 0 then 0.0
+             else if r < 1.0 then distr.cdf (r)
+             else 1.0
+    val diff = pl - pr 
+    val penalty = pr - pl - p // penalty if the interval is too small
+    diff * diff + penalty * penalty
+
+  assert { 0 <= l0 && l0 <= 1.0 }
+  val l = minimize (f, l0)
+  (l, distr.inverseCdf (distr.cdf (l) + p))
